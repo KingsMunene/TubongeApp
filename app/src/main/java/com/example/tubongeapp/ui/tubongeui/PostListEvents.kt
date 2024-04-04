451eca4f41ab606db62340411b169f1814cce6ac
KingsMunene <munenekings2@gmail.com>
2024-04-04 19:34:36 +0300

    Added a sealed class for post list events

--- app/src/main/java/com/example/tubongeapp/ui/tubongeui/PostListEvents.kt
+++ app/src/main/java/com/example/tubongeapp/ui/tubongeui/PostListEvents.kt
@@ -1,4 +1,13 @@
 package com.example.tubongeapp.ui.tubongeui
 
-class PostListEvents {
+import com.example.tubongeapp.data.Post
+
+sealed class PostListEvents {
+    data class PostClicked(val post: Post): PostListEvents()
+
+    data class CommentButtonClicked(val postId: Int): PostListEvents()
+
+    object NewPostClicked : PostListEvents()
+
+    data class OnLikeClicked(val postId: Int): PostListEvents()
 }
