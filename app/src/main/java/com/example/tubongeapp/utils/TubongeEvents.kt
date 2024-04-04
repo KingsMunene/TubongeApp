907ea6440a44964ed316fd52beea944bbf7abe05
KingsMunene <munenekings2@gmail.com>
2024-04-04 19:38:10 +0300

    Events for the application

---
+++ app/src/main/java/com/example/tubongeapp/utils/TubongeEvents.kt
@@ -0,0 +1,9 @@
+package com.example.tubongeapp.utils
+
+sealed class TubongeEvents {
+    data class Snackbar(val message: String, val action: String): TubongeEvents()
+
+    data class Navigate(val route: String): TubongeEvents()
+
+    object PopBackStack: TubongeEvents()
+}
